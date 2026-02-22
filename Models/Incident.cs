5be7cb1731a7a75e048b8f542c4649158421c5dc
Mohamed Abdouli <mohamed.abdouli2002@gmail.com>
2026-02-22 21:04:05 +0100

    Add Incident model

---
+++ Models/Incident.cs
@@ -0,0 +1,13 @@
+﻿namespace IncidentAPI_Abdouli.Models
+{
+    public class Incident
+    {
+        public int Id { get; set; } // int par défaut est 0
+        public String Title { get; set; } = null!; // Ou bien string.Empty
+        public String Description { get; set; } = string.Empty; // ou bien null!
+        public String Severity { get; set; } = string.Empty;
+        public String Status { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } // DateTime par défaut est 01/01/0001 00:00:00
+    }   
+}
+    
