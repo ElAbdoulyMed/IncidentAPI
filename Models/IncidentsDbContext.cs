f1f98b48a0e1542bd80f6488a1c76ef1ae7f0d94
Mohamed Abdouli <mohamed.abdouli2002@gmail.com>
2026-02-23 13:37:46 +0100

    Add IncidentsDbContext for Entity Framework Core

---
+++ Models/IncidentsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IncidentAPI_Abdouli.Models
+{
+    public class IncidentsDbContext : DbContext
+    {
+        public IncidentsDbContext(DbContextOptions<IncidentsDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Incident> Incidents { get; set; }
+    }
+}
